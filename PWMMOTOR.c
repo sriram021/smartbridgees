8a220eebbb9f2e31c55df8a8169f6ee8ddcd1fb9
sriram021 <51353695+sriram021@users.noreply.github.com>
2019-06-11 11:48:24 +0530

    Add files via upload

---
+++ PWMMOTOR.c
@@ -0,0 +1,40 @@
+/*
+ * pwmop.c
+ *
+ * Created: 10-06-2019 14:07:11
+ * Author : ramsr
+ */ 
+/*
+ * pwmop.c
+ *
+ * Created: 10-06-2019 14:07:11
+ * Author : ramsr
+ */ 
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+void PWM_init()
+{
+	TCCR0=(1<<WGM00)|(1<<WGM01)|(1<<COM01)|(1<<CS00);
+	DDRB|=(1<<PB3);
+}
+
+int main(void)
+{    DDRD=0xFF;
+    unsigned char duty;
+	PWM_init();
+    while (1) 
+    {
+		for(duty=0;duty<255;duty++)
+		{
+			OCR0=duty;
+			_delay_ms(8);
+			    }
+				for(duty=255;duty>1;duty--)
+				{
+					OCR0=duty;
+					_delay_ms(8);
+				}
+}
+}
+
